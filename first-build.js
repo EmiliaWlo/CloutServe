a055359537e185144bfdc645e4374efcea7e5a79
Emilia <emiliawlodarska@gmail.com>
2022-10-09 19:55:03 +0100

    Add chromedriver and selenium

---
+++ first-build.js
@@ -0,0 +1,14 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    await driver.get('https://www.google.com/ncr');
+    //remove the line below if not in EU
+    await driver.findElement(By.xpath("//*[ text() = 'Accept all' ]")).click()
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
